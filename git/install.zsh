06a80707cc3bf736fccfca62792b79e7dcc5a172
pdarulewski <pawel.darulewski@gmail.com>
2023-06-06 17:00:57 +0200

    feat: git config

--- git/install.zsh
+++ git/install.zsh
@@ -4,3 +4,6 @@ print_green "Installing git configurations."
 
 create_link "$PWD/git/.gitconfig" ${HOME}/.gitconfig
 create_link "$PWD/git/.gitignore" ${HOME}/.gitignore
+
+print_green "Setting github cli."
+gh config set editor nvim
