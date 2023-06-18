3b92456e23bff160a7812cd7e71248817879adcd
pdarulewski <pawel.darulewski@gmail.com>
2023-06-18 21:07:00 +0200

    feat: refactoring config

--- nvim/after/plugin/refactoring.lua
+++ nvim/after/plugin/refactoring.lua
@@ -3,4 +3,13 @@ if not ok then
   return
 end
 
-refactoring.setup({})
+require('refactoring').setup({
+  prompt_func_return_type = {
+    go = true,
+  },
+  prompt_func_param_type = {
+    go = true,
+  },
+  printf_statements = {},
+  print_var_statements = {},
+})
