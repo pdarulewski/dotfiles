1448c9708dcc085f0e47537b0ebb09b3374fd9f3
pdarulewski <pawel.darulewski@gmail.com>
2023-08-24 17:15:15 +0200

    fix: git without symlinks

--- git/after.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-print_green "Installing git configurations."
-
-create_link "$PWD/git/.gitconfig" $HOME/.gitconfig
-create_link "$PWD/git/.gitignore" $HOME/.gitignore
-
-print_green "Setting github cli."
-gh config set editor nvim
