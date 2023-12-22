7a50d5372f55319a94aa335cf60dac00b69560fb
Pawel Darulewski <pawel.darulewski@gmail.com>
2023-12-22 20:06:34 +0100

    feat: spicetify update

--- spicetify/after.zsh
+++ spicetify/after.zsh
@@ -1,7 +1,17 @@
 #!/usr/bin/env zsh
 
-git clone https://github.com/catppuccin/spicetify.git
-cp -r spicetify/catppuccin-* Themes/
-cp spicetify/js/* Extensions/
+git clone https://github.com/catppuccin/spicetify.git spicetify-catppuccin-tmp
+cp -r spicetify-catppuccin-tmp/catppuccin spicetify/Themes/
+rm -rdf spicetify-catppuccin-tmp
 
-spicetify backup apply
+spicetify config current_theme catppuccin
+spicetify config color_scheme frappe
+spicetify config inject_css 1 inject_theme_js 1 replace_colors 1 overwrite_assets 1
+spicetify apply
+
+git clone https://github.com/CharlieS1103/spicetify-extensions.git
+cp spicetify-extensions/adblock/adblock.js spicetify/Extensions
+spicetify config extensions adblock.js
+rm -rdf spicetify-extensions
+
+spicetify apply
