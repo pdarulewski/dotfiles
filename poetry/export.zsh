e41c6b5ccefc7dfc4547746121954f3333d38b9b
Pawel Darulewski <pawel.darulewski@gmail.com>
2024-02-11 11:39:56 +0100

    feat: export project env file

--- poetry/export.zsh
+++ poetry/export.zsh
@@ -3,16 +3,18 @@
 export POETRY_VIRTUALENVS_IN_PROJECT=1
 export POETRY_VIRTUALENVS_CREATE=1
 
-function vi() {
-  if [ -e .venv/ ]; then
-    . .venv/bin/activate
-    nvim $*;
-    deactivate;
-
-  else
-    nvim $*;
-
-  fi
-}
-
 alias s='. .venv/bin/activate'
+
+function vi() {
+	if [ -e .venv/ ]; then
+		envir
+		s
+		nvim $*
+		deactivate
+
+	else
+		envir
+		nvim $*
+
+	fi
+}
