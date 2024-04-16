dcc96d9a882fff01f843e87245212e05985508ff
Pawel Darulewski <pawel.darulewski@gmail.com>
2024-04-16 12:55:29 +0200

    feat: git clone maintenance

--- git/export.zsh
+++ git/export.zsh
@@ -28,3 +28,20 @@ alias gpuf='git push --force-with-lease'
 alias gsa='git stash --all'
 alias gsp='git stash pop'
 alias gsd='git stash drop'
+
+function gc() {
+	org=$(echo $1 | cut -d'/' -f4)
+	repo=$(echo $1 | cut -d'/' -f5 | cut -d'.' -f1)
+
+	git clone $1 $HOME/repo/$org/$repo
+
+	if [ ! -d $HOME/.config/git ]; then
+		mkdir $HOME/.config/git
+	fi
+	if [ ! -f $HOME/.config/git/maintenance ]; then
+		touch $HOME/.config/git/maintenance
+		echo "[maintenance]\n\tauto = false\n\tstrategy = incremental" >>$HOME/.config/git/maintenance
+	fi
+
+	echo "\trepo = $HOME/repo/$org/$repo" >>$HOME/.config/git/maintenance
+}
