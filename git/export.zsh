cd40ad912e6e7491fe5223c587820c5060123f78
Pawel Darulewski <pawel.darulewski@gmail.com>
2024-10-07 18:56:03 +0200

    chore: dist ignore

--- git/export.zsh
+++ git/export.zsh
@@ -14,7 +14,7 @@ alias gcmsg='git commit -m'
 
 # alias grbia='git rebase -i `git merge-base feature main`'
 function grbi() {
-	git rebase -i HEAD~$1
+  git rebase -i HEAD~$1
 }
 
 alias grbc='git rebase --continue'
@@ -30,26 +30,26 @@ alias gsp='git stash pop'
 alias gsd='git stash drop'
 
 function gc() {
-	# clone "git@github.com:pdarulewski/dotfiles.git" into ~/repo/github/pdarulewski/dotfiles
-	url=$(echo $1 | cut -d "@" -f 2 | cut -d ":" -f 1)
-	org=$(echo $1 | cut -d ":" -f 2 | cut -d "/" -f 1)
-	repo=$(basename -s .git $1)
+  # clone "git@github.com:pdarulewski/dotfiles.git" into ~/repo/github.com/pdarulewski/dotfiles
+  url=$(echo $1 | cut -d "@" -f 2 | cut -d ":" -f 1)
+  org=$(echo $1 | cut -d ":" -f 2 | cut -d "/" -f 1)
+  repo=$(basename -s .git $1)
 
-	git clone $1 $HOME/repo/$url/$org/$repo
+  git clone $1 $HOME/repo/$url/$org/$repo
 
-	if [ ! -d $HOME/.config/git ]; then
-		mkdir $HOME/.config/git
-	fi
-	if [ ! -f $HOME/.config/git/maintenance ]; then
-		touch $HOME/.config/git/maintenance
-		echo "[maintenance]\n\tauto = false\n\tstrategy = incremental" >>$HOME/.config/git/maintenance
-	fi
+  if [ ! -d $HOME/.config/git ]; then
+    mkdir $HOME/.config/git
+  fi
+  if [ ! -f $HOME/.config/git/maintenance ]; then
+    touch $HOME/.config/git/maintenance
+    echo "[maintenance]\n\tauto = false\n\tstrategy = incremental" >>$HOME/.config/git/maintenance
+  fi
 
-	echo "\trepo = $HOME/repo/$org/$repo" >>$HOME/.config/git/maintenance
+  echo "\trepo = $HOME/repo/$org/$repo" >>$HOME/.config/git/maintenance
 }
 
 function g() {
-	current_dir=$(pwd)
-	local repo_name=$(echo "$current_dir" | cut -d'/' -f5-7)
-	open "https://$repo_name"
+  current_dir=$(pwd)
+  local repo_name=$(echo "$current_dir" | cut -d'/' -f5-7)
+  open "https://$repo_name"
 }
