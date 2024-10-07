2ddfc5f1c1ebc88e54a6dc930a4807f21d97f0d9
Pawel Darulewski <pawel.darulewski@gmail.com>
2024-10-07 18:58:49 +0200

    feat: nvm

---
+++ nvm/export.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"                                       # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
