bea7a4ef4f15faa663f5ec4e11c1b263158b981e
Pawel Darulewski <pawel.darulewski@gmail.com>
2024-10-14 18:54:38 +0200

    feat: nix tmux

--- nix/modules/shared/home-manager.nix
+++ nix/modules/shared/home-manager.nix
@@ -3,5 +3,6 @@
 {
   git = import ./config/git.nix;
   kitty = import ./config/kitty.nix;
+  tmux = import ./config/tmux.nix { pkgs = pkgs; };
   zsh = import ./config/zsh.nix { pkgs = pkgs; lib = lib; config = config; };
 }
