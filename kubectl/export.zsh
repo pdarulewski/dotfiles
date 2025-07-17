1294f5cbcef352682d6e8a8357ce0c98a398ffa0
Pawel Darulewski <pawel.darulewski@gmail.com>
2025-07-17 17:26:00 +0200

    feat: kpf

--- kubectl/export.zsh
+++ kubectl/export.zsh
@@ -5,3 +5,12 @@ export KUBE_EDITOR="nvim"
 alias k='kubectl'
 alias kctx='kubectx'
 alias kns='kubens'
+
+function kpf() {
+  if [ -z "$1" ]; then
+    echo "Usage: kpf <pod-name>"
+    return 1
+  fi
+
+  kubectl port-forward "$1" 8080:8080
+}
