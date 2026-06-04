b72b7dd822259ae6fb43343ad594b9b9f34a2383
Pawel Darulewski <pawel.darulewski@gmail.com>
2026-06-04 17:47:39 +0200

    feat(git): worktree

--- modules/programs/git/func.zsh
+++ modules/programs/git/func.zsh
@@ -6,15 +6,38 @@ function grbi() {
 
 # clone "git@github.com:pdarulewski/dotfiles.git" into ~/dev/github.com/pdarulewski/dotfiles
 function gc() {
-  url=$(echo $1 | cut -d "@" -f 2 | cut -d ":" -f 1)
-  org=$(echo $1 | cut -d ":" -f 2 | cut -d "/" -f 1)
-  repo=$(basename -s .git $1)
+  local url=$(echo $1 | cut -d "@" -f 2 | cut -d ":" -f 1)
+  local org=$(echo $1 | cut -d ":" -f 2 | cut -d "/" -f 1)
+  local repo=$(basename -s .git $1)
+  local target="$HOME/dev/$url/$org/$repo"
 
-  git clone $1 $HOME/dev/$url/$org/$repo
+  git clone --bare $1 "$target/.bare"
+  echo "gitdir: ./.bare" >"$target/.git"
+  git -C "$target/.bare" config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+  git -C "$target/.bare" fetch origin
+  git -C "$target/.bare" worktree add "$target/main" main
 }
 
 function g() {
   current_dir=$(pwd)
   local repo_name=$(echo "$current_dir" | cut -d'/' -f5-7)
   open "https://$repo_name"
+}
+
+function gwa() {
+  local branch=$1
+  local dir=${branch//\//-}
+  git worktree add -b "$dir" "$branch"
+}
+
+function gwd() {
+  local branch=$1
+  local dir=${branch//\//-}
+  git worktree remove "$dir"
+}
+
+function gwp() {
+  local branch=$1
+  local dir=${branch//\//-}
+  git -C "$dir" pull origin "$branch"
 }
